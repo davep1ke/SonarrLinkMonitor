218d60058a33c35aa200dd0303aaf02ecad50907
philhornby <phil@hornbyweb.co.uk>
2015-09-27 11:53:39 +0100

    Initial Version
    Signed-off-by: philhornby <phil@hornbyweb.co.uk>

---
+++ SonarrLinkMonitor/Program.cs
@@ -0,0 +1,106 @@
+﻿
+using IWshRuntimeLibrary;
+using System.Text;
+using System;
+using System.Web;
+using System.Net;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SonarrLinkMonitor
+{
+    public class SonarrLinkMonitor
+    {
+
+
+        static void Main(string[] args)
+        {
+            SonarrLinkMonitor.CreateObject();
+        }
+
+        private static void CreateObject()
+        {
+            settings Settings = settings.load();
+
+            string URL = Settings.sonarrURL + "/api/history?page=1&pageSize=" + Settings.sonarrMaxHistory + "&sortkey=date&sortDir=desc&apikey=" + Settings.sonarrAPI;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            //request.ContentLength = DATA.Length;
+            
+
+            try
+            {
+                WebResponse webResponse = request.GetResponse();
+                using (Stream webStream = webResponse.GetResponseStream())
+                {
+                    if (webStream != null)
+                    {
+                        using (StreamReader responseReader = new StreamReader(webStream))
+                        {
+                                string response = responseReader.ReadToEnd();
+
+                                JObject jo = JObject.Parse(response);
+
+                                Console.Write(jo.First);
+
+
+                                //open the response and parse it using JSON. Query for newly imported files
+                                foreach (JToken token in jo.SelectTokens("records[*].data.importedPath")) //jo.Children()
+                                {
+                                    //go through these and create the link. 
+                                    bool found = false;
+                                    foreach (grabbedFile g in Settings.recentGrabs)
+                                    {
+                                        if (g.filename == token.ToString()) { found = true; }
+                                    }
+
+                                    if(found == true)
+                                    {
+                                        Console.Out.WriteLine("Already processed " + token.ToString());
+                                    }
+                                    else
+                                    {
+                                        Console.Out.WriteLine("Processing        " + token.ToString());
+                                        string filename = Settings.destinationFolder + @"/" + Path.GetFileNameWithoutExtension(token.ToString()) + ".lnk";
+                                        string destination = token.ToString();
+
+                                        //apply any replacemenbts
+                                        foreach (replacement r in Settings.replacements)
+                                        {
+                                            destination = destination.Replace(r.source, r.replace);
+                                        }
+
+                                        //craete the link file. 
+                                        var wsh = new IWshShell_Class();
+                                        IWshRuntimeLibrary.IWshShortcut shortcut = wsh.CreateShortcut(filename) as IWshRuntimeLibrary.IWshShortcut;
+                                        shortcut.TargetPath = destination;
+                                        shortcut.Save();
+
+                                        //log it
+                                        Settings.recentGrabs.Add(new grabbedFile(token.ToString()));
+                                    }
+                                }
+
+                            
+                            
+                        }
+                    }
+                }
+                Settings.save();
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine("-----------------");
+                Console.Out.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
---
+++ SonarrLinkMonitor/settings.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace SonarrLinkMonitor
+{
+    public class settings
+    {
+        public List<grabbedFile> recentGrabs = new List<grabbedFile>();
+        public List<replacement> replacements = new List<replacement>();
+
+        public string sonarrURL; //todo, make sure no trailing '/'
+        public string sonarrAPI;
+        public string destinationFolder; //todo, make sure no trailing '/'
+        public int sonarrMaxHistory = 200;
+        
+
+        public static settings load()
+        {
+            try
+            {
+                XmlSerializer deserializer = new XmlSerializer(typeof(settings));
+                TextReader textReader = new StreamReader(@"sett.ings");
+                settings setts = (settings)deserializer.Deserialize(textReader);
+                textReader.Close();
+                return setts;
+            }
+
+
+            catch (System.IO.FileNotFoundException)
+            {
+                //create a new one
+                settings sets = new settings();
+
+                #region populate defaults
+                // TODO remove
+                sets.sonarrURL = "http://davepine:8089";
+                sets.sonarrAPI = "8ef5ec83eaa041299ab5296dcb1ed36a";
+                sets.destinationFolder = @"C:\temp";
+
+                sets.replacements.Add(new replacement(@"F:\eps\", @"\\davepine\eps2\"));
+                sets.replacements.Add(new replacement(@"D:\eps\", @"\\davepine\eps\"));
+
+                #endregion
+                return sets;
+
+            }
+
+            catch (Exception e)
+            {
+                
+                string n = e.ToString();
+            }
+
+            return null;
+
+        }
+
+        public void save()
+        {
+
+            XmlSerializer serializer = new XmlSerializer(typeof(settings));
+            TextWriter textWriter = new StreamWriter(@"sett.ings");
+            serializer.Serialize(textWriter, this);
+            textWriter.Close();
+        }
+
+        /// <summary>
+        /// Throws away history after 1000 files
+        /// </summary>
+        public void trimHistory()
+        {
+            bool quitloop = false;
+            while (!quitloop)
+            {
+                if (recentGrabs.Count > 1000)
+                {
+                    Console.Out.WriteLine("Removing " + recentGrabs[0].filename + " from history");
+                    recentGrabs.RemoveAt(0);
+                    
+                }
+                else
+                {
+                    quitloop = true;
+                }
+
+            }
+
+        }
+
+    }
+
+
+
+}
